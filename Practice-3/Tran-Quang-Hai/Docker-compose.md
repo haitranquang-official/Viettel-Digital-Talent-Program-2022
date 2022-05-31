cfafddcb753fe374dc05f2e87f45d64605885ba3
Hai Tran Quang <75372438+haitranquang-official@users.noreply.github.com>
2022-05-31 19:19:59 +0700

    Initial md file

---
+++ Practice-3/Tran-Quang-Hai/Docker-compose.md
@@ -0,0 +1,17 @@
+# **TASK: CREATE A THREE-TIER APPLICATION AND DEPLOY WITH DOCKER AND DOCKER-COMPOSE**
+
+## **1. Overview**
+
+### **1.1.** 
+
+## **2. MongoDB**
+
+### **2.1.**
+
+## **3. Python Application**
+
+### **3.1.**
+
+## **4. Nginx**
+
+### **4.1.**
--- Practice-3/Tran-Quang-Hai/python-http/app.py
+++ Practice-3/Tran-Quang-Hai/python-http/app.py
@@ -3,22 +3,23 @@ from flask import jsonify, request, Flask
 from flask_mongoengine import MongoEngine
 app = Flask(__name__)
 app.config['MONGODB_SETTINGS'] = config.get_config()
-print(config.get_config())
-print(app.config)
-print('Hello')
 db = MongoEngine()
 db.init_app(app)
+
 
 class Student(db.Document):
     name = db.StringField()
     email = db.StringField()
+
     def to_json(self):
         return {"name": self.name,
                 "email": self.email}
 
+
 @app.route('/all', methods=['GET'])
 def all():
     return jsonify(Student.objects.all())
+
 
 @app.route('/add', methods=['POST'])
 def add():
@@ -26,6 +27,7 @@ def add():
     student = Student(name=record['name'], email=record['email'])
     student.save()
     return jsonify(student.to_json())
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
